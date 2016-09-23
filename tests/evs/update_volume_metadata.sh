5900503c8e88778d328f190c149cf710adac1720
Radoslav Kuzma <radoslav.kuzma@gmail.com>
2016-09-23 15:01:10 +0000

    Added test files

---
+++ tests/evs/update_volume_metadata.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+VER=1.1
+
+apitest ()
+{
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]; then
+        echo "error with $1" >&2
+        echo "$@ => NOT OK " >>"native_test_result_$VER.txt"
+    else
+        echo "$@ => OK " >>"native_test_result_$VER.txt"
+    fi
+    return $status
+
+}
+
+echo "NATIVE TEST $VER" >"native_test_result_$VER.txt"
+
+
+VOLUME_NAME=$(cat /dev/urandom | tr -dc 'A-Za-z' | fold -w 10 | head -n 1)_VOLUME
+
+apitest cinder --insecure create --name ${VOLUME_NAME} 5 2>/dev/null
+sleep 10
+apitest cinder --insecure metadata-show ${VOLUME_NAME} 2>/dev/null
+apitest cinder --insecure metadata ${VOLUME_NAME} set aaa=bbb 2>/dev/null
+sleep 5
+apitest cinder --insecure metadata-show ${VOLUME_NAME} 2>/dev/null
+apitest cinder --insecure delete ${VOLUME_NAME} 2>/dev/null
