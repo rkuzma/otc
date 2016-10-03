885ae1a5140ac73e0780f4c7a0352085bd1b076e
Radoslav Kuzma <radoslav.kuzma@gmail.com>
2016-10-03 11:36:23 +0000

    Adjusted headers

--- tests/tests-native/2.1_list_networks.sh
+++ tests/tests-native/2.1_list_networks.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
-VER=1.1
 
-apitest ()
-{
-    "$@"
-    local status=$?
-    if [ $status -ne 0 ]; then
-        echo "error with $1" >&2
-        echo "$@ => NOT OK " >>"native_test_result_$VER.txt"
-    else
-        echo "$@ => OK " >>"native_test_result_$VER.txt"
-    fi
-    return $status
-
-}
-
-echo "NATIVE TEST $VER" >"native_test_result_$VER.txt"
+source otcfunc.sh
 
 apitest neutron --insecure net-list 2>/dev/null
